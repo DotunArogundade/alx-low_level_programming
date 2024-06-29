e9dd47a526c2788890c706d81679b13010196f3e
dotunarogundade <dotman227@gmail.com>
2024-06-29 14:06:22 +0100

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Description: A simple program that prints a specific message 
+ * Description: A simple program that prints a specific message
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
