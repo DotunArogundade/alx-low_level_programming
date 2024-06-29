6580185a5235ecb7bde842e1fc689d667a87ac3a
dotunarogundade <dotman227@gmail.com>
2024-06-29 13:53:00 +0100

    a C program that prints a variable

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A simple program that prints a specific message 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle");
+	return (0);
+}
