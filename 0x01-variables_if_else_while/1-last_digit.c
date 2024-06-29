1ea21637a12c7f80e3c97591bcf5797d4ea1e78f
dotunarogundade <dotman227@gmail.com>
2024-06-29 22:34:42 +0100

    add semicolon

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@ int main(void)
 	int last_digit;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2
+	n = rand() - RAND_MAX / 2;
 
 	last_digit = n % 10;
 
-	printf("last digit of %d is %d ", n, last_digit);
+	printf("last digit of %d is %d and is ", n, last_digit);
 	if (last_digit > 5)
 	{
 		printf("and is greater than 5\n");
