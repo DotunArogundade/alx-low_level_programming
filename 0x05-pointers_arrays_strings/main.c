98b0134f4a9ea7364025b419b29aaf53f006c7a6
dotunarogundade <dotman227@gmail.com>
2024-07-01 16:47:42 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - ENtry point to test the reset_to_98 function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 50;
+
+	reset_to_98(&num);
+
+	_putchar('0' + (num / 10));
+	_putchar('0' + (num % 10));
+	_putchar('n');
+
+	return (0);
+}
