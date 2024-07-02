24c98e18541d0828e1cbd1af8ad3323fc0969e52
dotunarogundade <dotman227@gmail.com>
2024-07-02 10:37:11 +0100

    A function that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test the _puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
