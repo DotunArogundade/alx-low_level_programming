c372036d08a0352a59fef47ef73672f535e6ea1f
dotunarogundade <dotman227@gmail.com>
2024-07-09 00:58:59 +0100

    A functiointhat locates a character in a string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ int _isupper(int c);
 int mul(int a, int b);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, const char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
