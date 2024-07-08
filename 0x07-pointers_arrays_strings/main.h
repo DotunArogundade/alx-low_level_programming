adcfd3afda0e2c1dcbce46811f2c8df1273050db
dotunarogundade <dotman227@gmail.com>
2024-07-09 00:44:36 +0100

    A function that copies memory area

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ int _isdigit(int c);
 int _isupper(int c);
 int mul(int a, int b);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, const char *src, unsigned int n);
 
 #endif
