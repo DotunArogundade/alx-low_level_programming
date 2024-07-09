4bbed3d53e8c7a665b15825910a93155c8e6765f
dotunarogundade <dotman227@gmail.com>
2024-07-09 01:14:51 +0100

    A funtion thar gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ int mul(int a, int b);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, const char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
